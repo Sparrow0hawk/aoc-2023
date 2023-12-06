520696d5c35f097dca9283ff4d487c34bd77b439
Sparrow0hawk <alexjcoleman@gmail.com>
2023-12-06 08:39:57 +0000

    Correct print for day 4 part 1

--- aoc/day4/part1.py
+++ aoc/day4/part1.py
@@ -5,4 +5,4 @@ data = "data/day4.txt"
 with open(data) as open_file:
     cards = [Card(line=line) for line in open_file.readlines()]
 
-print(f"The solution to day 3 part 1 is {sum([card.points for card in cards])}")
+print(f"The solution to day 4 part 1 is {sum([card.points for card in cards])}")
